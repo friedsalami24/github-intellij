5a47b01fe986546a330506bcafed9b9f1de3c8b0
uwais <uwaisromzi@gmail.com>
2025-11-26 22:44:28 +0800

    1)finalizing customer flow 2)working on order calculation logic

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,12 @@ public class Main {
 
         String choice = scanner.nextLine().trim();
 
-        if(choice.equals(1)) {
+        if(choice.equals("1")) {
             //CALLING CUSTOMER FLOW
-            //Customer customer = customerFlow(scanner);
+            Customer customer = custFlow(scanner);
 
             System.out.println("\n=== CUSTOMER FLOW STARTING ===");
-        } else if (choice.equals(2)) {
+        } else if (choice.equals("2")) {
             //CALLING ADMIN FLOW
             Administrator admin = adminFlow(scanner);
 
@@ -29,8 +29,6 @@ public class Main {
         }
 
     }
-
-
 
     //CREATING ADMIN FLOW CLASS
     private static Administrator adminFlow(Scanner scanner) {
@@ -42,6 +40,8 @@ public class Main {
         System.out.println("Enter Email :  ");
         String adminEmail = scanner.nextLine();
 
+        System.out.println("\n=== ADMIN LOG IN SUCCESSFUL");
+
         //INITIALIZING ADMIN OBJECT OF CLASS ADMINISTRATOR TO PASS USERNAME AND EMAIL
         Administrator admin1 = new Administrator(adminUsername, adminEmail);
 
@@ -51,11 +51,15 @@ public class Main {
         admin1.setDepartment(adminDepartment); //adding department to existing admin object
 
         admin1.displayProfile();
+
+        return admin1;
     }
 
+    //CREATING CUSTOMER FLOW CLASS
     private static Customer custFlow(Scanner scanner) {
         System.out.println("\n=== CUSTOMER LOGIN ===");
 
+        //PHASE 1 (CUSTOMER USERNAME AND EMAIL PROMPT)
         System.out.println("Enter Username : ");
         String custName = scanner.nextLine().trim();
 
@@ -63,9 +67,56 @@ public class Main {
         String custEmail = scanner.nextLine().trim();
 
         Customer c1 = new Customer(custName, custEmail);
+
+        System.out.println("\n=== CUSTOMER LOG IN SUCCESSFUL");
+
+        //PHASE 2 (TAKING CUSTOMERS ORDER)
+        System.out.println("\n=== STARTING ORDER ===");
+
+        Product.displayCatalog();
+
+        List<Product> selectedProducts = new ArrayList<>();
+        String productSelection;
+        boolean orderProcess = true;
+        String purchaseDate = "";
+
+        while(orderProcess) {
+            System.out.println("Enter product name to add (or type 'done'");
+            productSelection = scanner.nextLine().trim();
+
+            if (productSelection.equalsIgnoreCase("done")) {
+                System.out.println("Enter date (eg; 2025-10-29) : ");
+                purchaseDate = scanner.nextLine().trim();
+                orderProcess = false; //exit loop, end program
+            }
+            else if (Product.isProductAvailable(productSelection)) {
+                Product chosenProduct = Product.getProductByName(productSelection);
+                //adding products to array list
+                selectedProducts.add(chosenProduct);
+                System.out.println(productSelection + " has been added to cart.");
+            }
+            else {
+                System.out.println("Product typed not found, please try again.");
+            }
+        }
+
+        //PHASE 3
+        if (!selectedProducts.isEmpty()) {
+            Order o1 = new Order(c1, selectedProducts, purchaseDate);
+            o1.displayOrder();
+            System.out.println("Order total with 5% tax: $" + o1.calculateTotal(0.05));
+
+            System.out.println("\nThank you for your order! Session complete.");
+        }
+        else {
+            System.out.println("No order selected, order cancelled. Bye");
+        }
+
+        return c1;
     }
 
-    private static void productInitialization(Scanner scanner) {
+    //CREATING PRODUCT INITIALIZATION TO CREATE AND STORE PRODUCTS IN MAP FOR DISPLAY
+    private static void productInitialization() {
         //Test Product (Static counter)
         Product p1 = new Product("Laptop", 1200.00, "SKU1001");
         Product p2 = new Product("Mouse", 24.50, "SKU1002");
@@ -84,72 +135,27 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
+        //ADDING PRODUCTS TO CATALOG
+        productInitialization();
 
-        //Test Product (Static counter)
-        Product p1 = new Product("Laptop", 1200.00, "SKU1001");
-        Product p2 = new Product("Mouse", 24.50, "SKU1002");
-        Product p3 = new Product("Keyboard", 100.00, "SKU1003");
-        Product p4 = new Product("Headset", 150.00, "SKU1004");
-        //System.out.println("Total products cataloged: " + Product.getTotalProducts());
+        //STARTING PROCESS
+        appFlow(scanner);
 
-
-        //THIS BLOCK IS TO ADD CREATED PRODUCTS OBJECT TO DISPLAYMAP FOR CATALOG DISPLAY
-        Product.addToCatalog(p1);
-        Product.addToCatalog(p2);
-        Product.addToCatalog(p3);
-        Product.addToCatalog(p4);
-
-        System.out.println("Total products cataloged: " + Product.getTotalProducts());
-
-        //USER SELECTION LOGIC
-        System.out.println("\n--- Starting New Order ---");
-        Product.displayCatalog();
-
-        List<Product> selectedProducts = new ArrayList<>();
-        String productSelection;
-        boolean orderProcess = true;
-
-        while(orderProcess) {
-            System.out.println("Enter product name to add (or type 'done'");
-            productSelection = scanner.nextLine().trim();
-
-            if (productSelection.equalsIgnoreCase("done")) {
-                orderProcess = false; //exit loop, end program
-            }
-            else if (Product.isProductAvailable(productSelection)) {
-                Product chosenProduct = Product.getProductByName(productSelection);
-                selectedProducts.add(chosenProduct);
-                System.out.println(productSelection + " has been added to cart.");
-            }
-            else {
-                System.out.println("Product typed not found, please try again.");
-            }
-        }
-
-        /*if (!selectedProducts.isEmpty()) {
-            // Replace the hardcoded list with the dynamic list:
-            Order o1 = new Order(c1, selectedProducts, "2025-10-25");
-
-            o1.displayOrder();
-            System.out.println("Order total with 5% tax: $" + o1.calculateTotal(0.05));
-        } else {
-            System.out.println("Order cancelled. No items selected.");
-        }*/
-
+        //CLOSING SCANNER
+        scanner.close();
 
         //Test Order(initializer blocks, interface)
         /*Order o1 = new Order(c1, List.of(p1,p2), "2025-10-25");
         o1.displayOrder();
-        System.out.println("Order total with 5% tax: $" + o1.calculateTotal(0.05));*/
+        System.out.println("Order total with 5% tax: $" + o1.calculateTotal(0.05));
         Order o2 = new Order(c2, List.of(p3,p4), "2027-10-09");
         o2.displayOrder();
-        System.out.println("Order total with 5% tax: $" + o2.calculateTotal(0.05));
+        System.out.println("Order total with 5% tax: $" + o2.calculateTotal(0.05));*/
 
         //Test Nested Class
-        ECommerce.Inventory mainWarehouse = new ECommerce.Inventory("Main Warehouse");
-        mainWarehouse.checkStock(p1);
+        /*ECommerce.Inventory mainWarehouse = new ECommerce.Inventory("Main Warehouse");
+        mainWarehouse.checkStock(p1);*/
     }
 }
